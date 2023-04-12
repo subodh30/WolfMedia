1ff0a34f14b95d5a87f1630d7b1fbaee169df970
ameyagv <ameyav1234@gmail.com>
2023-04-12 19:54:39 -0400

    added more report operations

--- src/main/java/edu/ncsu/dbms/wolfmedia/controllers/ReportOperationController.java
+++ src/main/java/edu/ncsu/dbms/wolfmedia/controllers/ReportOperationController.java
@@ -3,6 +3,8 @@ package edu.ncsu.dbms.wolfmedia.controllers;
 import edu.ncsu.dbms.wolfmedia.models.SongHistory;
 import edu.ncsu.dbms.wolfmedia.services.ReportOperationService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -32,7 +34,42 @@ public class ReportOperationController {
     }
 
     @GetMapping("/calculateHostPayments")
-    public Double calculateHostPayments() {
-        return reportOperationService.calculateHostPayments("2023-01-01", "2023-03-01");
+    public Double calculateHostPayments(@RequestParam String startDate, @RequestParam String endDate) {
+        return reportOperationService.calculateHostPayments(startDate, endDate);
+    }
+
+    @GetMapping("/calculateArtistPayments")
+    public Double calculateArtistPayments(@RequestParam String startDate, @RequestParam String endDate) {
+        return reportOperationService.calculateArtistPayments(startDate, endDate);
+    }
+
+    @GetMapping("/calculateRecordLabelPayments")
+    public Double calculateRecordLabelPayments(@RequestParam String startDate, @RequestParam String endDate) {
+        return reportOperationService.calculateRecordLabelPayments(startDate, endDate);
+    }
+
+    @GetMapping("/calculateMonthlyRevenue")
+    public List<Map<String, Object>> calculateMonthlyRevenue() {
+        return reportOperationService.calculateMonthlyRevenue();
+    }
+
+    @GetMapping("/calculateYearlyRevenue")
+    public List<Map<String, Object>> calculateYearlyRevenue() {
+        return reportOperationService.calculateYearlyRevenue();
+    }
+
+    @GetMapping("/songsByArtist/{artistId}")
+    public List<Map<String, Object>> getSongsByArtist(@PathVariable("artistId") int artistId) {
+        return reportOperationService.getSongsByArtist(artistId);
+    }
+
+    @GetMapping("/getSongsByAlbum/{albumId}")
+    public List<Map<String, Object>> getSongsByAlbum(@PathVariable("albumId") int albumId) {
+        return reportOperationService.getSongsByAlbum(albumId);
+    }
+
+    @GetMapping("/getPodcastEpisodesByPodcast/{podcastId}")
+    public List<Map<String, Object>> getPodcastEpisodesByPodcast(@PathVariable("podcastId") int podcastId) {
+        return reportOperationService.getPodcastEpisodesByPodcast(podcastId);
     }
 }
--- src/main/java/edu/ncsu/dbms/wolfmedia/services/ReportOperationService.java
+++ src/main/java/edu/ncsu/dbms/wolfmedia/services/ReportOperationService.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Service;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 
@@ -81,6 +83,123 @@ public class ReportOperationService {
             e.printStackTrace();
         }
         return totalPayment;
+    }
+
+    public Double calculateArtistPayments(String startDate, String endDate) {
+        ResultSet data = genericDAO.executeQuery("SELECT SUM(amount) FROM distributesRoyalties WHERE date BETWEEN '" + startDate + "' and '" + endDate + "'");
+        Double totalPayment = 0.0;
+        try {
+            while (data.next()) {
+                totalPayment = Double.valueOf(new DecimalFormat("#.##").format(data.getDouble(1)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return totalPayment;
+    }
+
+    public Double calculateRecordLabelPayments(String startDate, String endDate) {
+        ResultSet data = genericDAO.executeQuery("SELECT 0.3*SUM(amount) AS 'Record Label Payments' FROM receives INNER JOIN serviceAccount ON receives.transactionId = serviceAccount.transactionId WHERE date BETWEEN '" + startDate + "' and '" + endDate + "'");
+        Double totalPayment = 0.0;
+        try {
+            while (data.next()) {
+                totalPayment = Double.valueOf(new DecimalFormat("#.##").format(data.getDouble(1)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return totalPayment;
+    }
+
+    public List<Map<String, Object>> calculateMonthlyRevenue() {
+        ResultSet data = genericDAO.executeQuery("SELECT YEAR(date) AS year, MONTH(date) AS month, SUM(CASE WHEN type = 'Credit' THEN amount ELSE 0 END) AS revenue FROM serviceAccount GROUP BY YEAR(date), MONTH(date);");
+
+        List<Map<String, Object>> revenueList = new ArrayList<>();
+        try {
+            while (data.next()) {
+                revenueList.add(new HashMap<>());
+                Map<String, Object> map = revenueList.get(revenueList.size() - 1);
+                map.put("year", data.getInt(1));
+                map.put("month", data.getInt(2));
+                map.put("Monthly Revenue", new DecimalFormat("#.##").format(data.getDouble(3)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return revenueList;
+    }
+
+    public List<Map<String, Object>> calculateYearlyRevenue() {
+        ResultSet data = genericDAO.executeQuery("SELECT YEAR(date) AS year, SUM(CASE WHEN type = 'Credit' THEN amount ELSE 0 END) AS revenue FROM serviceAccount GROUP BY YEAR(date);");
+
+        List<Map<String, Object>> revenueList = new ArrayList<>();
+        try {
+            while (data.next()) {
+                revenueList.add(new HashMap<>());
+                Map<String, Object> map = revenueList.get(revenueList.size() - 1);
+                map.put("year", data.getInt(1));
+                map.put("Yearly Revenue", new DecimalFormat("#.##").format(data.getDouble(2)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return revenueList;
+    }
+
+    public List<Map<String, Object>> getSongsByArtist(int artistId) {
+        ResultSet data = genericDAO.executeQuery("SELECT distinct(songs.songId), title FROM songs LEFT JOIN creates ON songs.songId = creates.songId WHERE artistId = " + artistId + " OR primaryArtist = " + artistId + ";");
+
+        List<Map<String, Object>> songs = new ArrayList<>();
+        try {
+            while (data.next()) {
+                songs.add(new HashMap<>());
+                Map<String, Object> map = songs.get(songs.size() - 1);
+                map.put("songId", data.getInt(1));
+                map.put("songTitle", data.getString(2));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return songs;
+    }
+
+    public List<Map<String, Object>> getSongsByAlbum(int albumId) {
+        ResultSet data = genericDAO.executeQuery("SELECT songId, title from songs WHERE albumId = " + albumId + ";");
+
+        List<Map<String, Object>> songs = new ArrayList<>();
+        try {
+            while (data.next()) {
+                songs.add(new HashMap<>());
+                Map<String, Object> map = songs.get(songs.size() - 1);
+                map.put("songId", data.getInt(1));
+                map.put("songTitle", data.getString(2));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return songs;
+    }
+
+    public List<Map<String, Object>> getPodcastEpisodesByPodcast(int podcastId) {
+        ResultSet data = genericDAO.executeQuery("SELECT * FROM episodes WHERE podcastId = " + podcastId + ";");
+
+        List<Map<String, Object>> episodes = new ArrayList<>();
+        try {
+            while (data.next()) {
+                episodes.add(new HashMap<>());
+                Map<String, Object> map = episodes.get(episodes.size() - 1);
+                map.put("podcastId", data.getInt(1));
+                map.put("number", data.getInt(2));
+                map.put("title", data.getString(3));
+                map.put("duration", data.getDouble(4));
+                map.put("releaseDate", data.getString(5));
+                map.put("listeningCount", data.getInt(6));
+                map.put("advertisementCount", data.getInt(7));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return episodes;
     }
 
 }

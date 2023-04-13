3764d9187ef903b9774dbcb10a6de92578664bc3
Dhrumil Shah <dhrumilshah0310@gmail.com>
2023-04-13 14:02:03 -0400

    Metadata file bugs fixed

--- src/main/java/edu/ncsu/dbms/wolfmedia/controllers/MetadataController.java
+++ src/main/java/edu/ncsu/dbms/wolfmedia/controllers/MetadataController.java
@@ -20,62 +20,69 @@ public class MetadataController {
     }
 
     @GetMapping("/addPlayCountSong")
-    public Boolean addPlayCountSong(@RequestParam Integer songId, @RequestParam Integer playCount)  throws Exception{
+    public Boolean addPlayCountSong(@RequestParam Integer songId, @RequestParam Integer playCount) throws Exception {
         return metadataService.addPlayCountSong(songId, playCount);
     }
 
     @GetMapping("/addMonthlyListeners")
-    public Boolean addMonthlyListeners(@RequestParam Integer artistId, @RequestParam Integer monthlyListeners) throws Exception {
+    public Boolean addMonthlyListeners(@RequestParam Integer artistId, @RequestParam Integer monthlyListeners)
+            throws Exception {
         return metadataService.addMonthlyListeners(artistId, monthlyListeners);
     }
 
     @GetMapping("/addTotalSubscribers")
-    public Boolean addTotalSubscribers(@RequestParam Integer podcastId, @RequestParam Integer totalSubscribers) throws Exception {
+    public Boolean addTotalSubscribers(@RequestParam Integer podcastId, @RequestParam Integer totalSubscribers)
+            throws Exception {
         return metadataService.addTotalSubscribers(podcastId, totalSubscribers);
     }
 
     @GetMapping("/addPodcastRatings")
-    public Boolean addPodcastRatings(@RequestParam Integer podcastId, @RequestParam Integer rating)  throws Exception{
+    public Boolean addPodcastRatings(@RequestParam Integer podcastId, @RequestParam Integer rating) throws Exception {
         return metadataService.addPodcastRatings(podcastId, rating);
     }
 
     @GetMapping("/addPodcastEpisodeListenerCount")
-    public Boolean addPodcastEpisodeListenerCount(@RequestParam Integer podcastId, @RequestParam Integer episodeId, Integer listenerCount) throws Exception {
-        return metadataService.addPodcastEpisodeListenerCount(podcastId, episodeId, listenerCount);
+    public Boolean addPodcastEpisodeListenerCount(@RequestParam Integer podcastId, @RequestParam Integer number,
+            Integer ListeningCount) throws Exception {
+        return metadataService.addPodcastEpisodeListenerCount(podcastId, number, ListeningCount);
     }
 
     @GetMapping("/updatePlayCountSong")
-    public Boolean updatePlayCountSong(@RequestParam Integer songId, @RequestParam Integer playCount)  throws Exception{
+    public Boolean updatePlayCountSong(@RequestParam Integer songId, @RequestParam Integer playCount) throws Exception {
         return metadataService.updatePlayCountSong(songId, playCount);
     }
 
     @GetMapping("/updateMonthlyListeners")
-    public Boolean updateMonthlyListeners(@RequestParam Integer artistId, @RequestParam Integer monthlyListeners)  throws Exception{
+    public Boolean updateMonthlyListeners(@RequestParam Integer artistId, @RequestParam Integer monthlyListeners)
+            throws Exception {
         return metadataService.updateMonthlyListeners(artistId, monthlyListeners);
     }
 
     @GetMapping("/updateTotalSubscribers")
-    public Boolean updateTotalSubscribers(@RequestParam Integer podcastId, @RequestParam Integer totalSubscribers) throws Exception {
+    public Boolean updateTotalSubscribers(@RequestParam Integer podcastId, @RequestParam Integer totalSubscribers)
+            throws Exception {
         return metadataService.updateTotalSubscribers(podcastId, totalSubscribers);
     }
 
     @GetMapping("/updatePodcastRatings")
-    public Boolean updatePodcastRatings(@RequestParam Integer podcastId, @RequestParam Integer rating)  throws Exception{
+    public Boolean updatePodcastRatings(@RequestParam Integer podcastId, @RequestParam Integer rating)
+            throws Exception {
         return metadataService.updatePodcastRatings(podcastId, rating);
     }
 
     @GetMapping("/updatePodcastEpisodeListenerCount")
-    public Boolean updatePodcastEpisodeListenerCount(@RequestParam Integer podcastId, @RequestParam Integer episodeId, @RequestParam Integer listenerCount) throws Exception {
-        return metadataService.updatePodcastEpisodeListenerCount(podcastId, episodeId, listenerCount);
+    public Boolean updatePodcastEpisodeListenerCount(@RequestParam Integer podcastId, @RequestParam Integer number,
+            @RequestParam Integer ListeningCount) throws Exception {
+        return metadataService.updatePodcastEpisodeListenerCount(podcastId, number, ListeningCount);
     }
 
     @GetMapping("/findSongsByArtist")
-    public List<Map<String, Object>> findSongsByArtist(@RequestParam Integer artistId)  throws Exception{
+    public List<Map<String, Object>> findSongsByArtist(@RequestParam Integer artistId) throws Exception {
         return metadataService.findSongsByArtist(artistId);
     }
 
     @GetMapping("/findSongsByAlbum")
-    public List<Songs> findSongsByAlbum(@RequestParam Integer albumId)  throws Exception{
+    public List<Songs> findSongsByAlbum(@RequestParam Integer albumId) throws Exception {
         return metadataService.findSongsByAlbum(albumId);
     }
 
--- src/main/java/edu/ncsu/dbms/wolfmedia/services/MetadataService.java
+++ src/main/java/edu/ncsu/dbms/wolfmedia/services/MetadataService.java
@@ -21,94 +21,103 @@ public class MetadataService {
     }
 
     public Boolean addPlayCountSong(Integer songId, Integer playCount) throws Exception {
-            return genericDAO.executeUpdate("UPDATE songs SET playCount = "+playCount+"WHERE songId = "+songId);
+        return genericDAO.executeUpdate("UPDATE songs SET playCount = " + playCount + " WHERE songId = " + songId);
     }
 
     public Boolean addMonthlyListeners(Integer artistId, Integer monthlyListeners) throws Exception {
-            String query = "UPDATE artists SET monthlyListeners = "+monthlyListeners+"WHERE artistId = "+artistId;
-            return genericDAO.executeUpdate(query);
+        String query = "UPDATE artists SET monthlyListeners = " + monthlyListeners + " WHERE artistId = " + artistId;
+        return genericDAO.executeUpdate(query);
 
     }
 
-    public Boolean addTotalSubscribers(Integer podcastId, Integer totalSubscribers)  throws Exception{
-            String query = "UPDATE podcasts SET totalSubscribers = "+totalSubscribers+"WHERE podcastId = "+podcastId;
-          return genericDAO.executeUpdate(query);
+    public Boolean addTotalSubscribers(Integer podcastId, Integer totalSubscribers) throws Exception {
+        String query = "UPDATE podcasts SET totalSubscribers = " + totalSubscribers + " WHERE podcastId = " + podcastId;
+        return genericDAO.executeUpdate(query);
     }
 
     public Boolean addPodcastRatings(Integer podcastId, Integer rating) throws Exception {
-            String query = "UPDATE podcasts SET rating = "+rating+"WHERE podcastId = "+podcastId;
-            return genericDAO.executeUpdate(query);
+        String query = "UPDATE podcasts SET rating = " + rating + " WHERE podcastId = " + podcastId;
+        return genericDAO.executeUpdate(query);
 
     }
 
-    public Boolean addPodcastEpisodeListenerCount(Integer podcastId, Integer episodeId, Integer listenerCount) throws Exception {
+    public Boolean addPodcastEpisodeListenerCount(Integer podcastId, Integer number, Integer ListeningCount)
+            throws Exception {
 
-            String query = "UPDATE episodes SET ListenerCount = "+listenerCount+"WHERE podcastId = "+podcastId+" AND number = "+episodeId;
-            return genericDAO.executeUpdate(query);
+        String query = "UPDATE episodes SET ListeningCount = " + ListeningCount + " WHERE podcastId = " + podcastId
+                + " AND number = " + number;
+        return genericDAO.executeUpdate(query);
     }
 
     public Boolean updatePlayCountSong(Integer songId, Integer playCount) throws Exception {
-            String query = "UPDATE songs SET playCount = "+playCount+"WHERE songId = "+songId;
-            return genericDAO.executeUpdate(query);
+        String query = "UPDATE songs SET playCount = " + playCount + " WHERE songId = " + songId;
+        return genericDAO.executeUpdate(query);
     }
 
     public Boolean updateMonthlyListeners(Integer artistId, Integer monthlyListeners) throws Exception {
-            String query = "UPDATE artists SET monthlyListeners = "+monthlyListeners+"WHERE artistId = "+artistId;
-            return genericDAO.executeUpdate(query);
+        String query = "UPDATE artists SET monthlyListeners = " + monthlyListeners + " WHERE artistId = " + artistId;
+        return genericDAO.executeUpdate(query);
     }
 
-    public Boolean updateTotalSubscribers(Integer podcastId, Integer totalSubscribers)  throws Exception{
-            String query = "UPDATE podcasts SET totalSubscribers = "+totalSubscribers+"WHERE podcastId = "+podcastId;
-            return genericDAO.executeUpdate(query);
+    public Boolean updateTotalSubscribers(Integer podcastId, Integer totalSubscribers) throws Exception {
+        String query = "UPDATE podcasts SET totalSubscribers = " + totalSubscribers + " WHERE podcastId = " + podcastId;
+        return genericDAO.executeUpdate(query);
     }
 
-    public Boolean updatePodcastRatings(Integer podcastId, Integer rating)  throws Exception{
+    public Boolean updatePodcastRatings(Integer podcastId, Integer rating) throws Exception {
 
-
-            String query = "UPDATE podcasts SET rating = "+rating+"WHERE podcastId = "+podcastId;
-            return genericDAO.executeUpdate(query);
+        String query = "UPDATE podcasts SET rating = " + rating + " WHERE podcastId = " + podcastId;
+        return genericDAO.executeUpdate(query);
     }
 
-    public Boolean updatePodcastEpisodeListenerCount(Integer podcastId, Integer episodeId, Integer listenerCount)  throws Exception{
-            String query = "UPDATE episodes SET ListenerCount = "+listenerCount+"WHERE podcastId = "+podcastId+" AND number = "+episodeId;
-            return genericDAO.executeUpdate(query);
+    public Boolean updatePodcastEpisodeListenerCount(Integer podcastId, Integer number, Integer ListeningCount)
+            throws Exception {
+        String query = "UPDATE episodes SET ListeningCount = " + ListeningCount + " WHERE podcastId = " + podcastId
+                + " AND number = " + number;
+        return genericDAO.executeUpdate(query);
     }
 
-    public List<Map<String, Object>> findSongsByArtist(Integer artistId)  throws Exception{
-            String query = "SELECT distinct(songs.songId), title FROM songs LEFT JOIN creates ON songs.songId = creates.songId WHERE artistId = "+artistId+" OR primaryArtist = "+artistId;
-            ResultSet rs = genericDAO.executeQuery(query);
-            List<Map<String, Object>> result = new ArrayList<>();
-            try {
-                while (rs.next()) {
-                    result.add(Map.of("songId", rs.getString(1), "title", rs.getString(2)));
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+    public List<Map<String, Object>> findSongsByArtist(Integer artistId) throws Exception {
+        String query = "SELECT distinct(songs.songId), title FROM songs LEFT JOIN creates ON songs.songId = creates.songId WHERE artistId = "
+                + artistId + " OR primaryArtist = " + artistId;
+        ResultSet rs = genericDAO.executeQuery(query);
+        List<Map<String, Object>> result = new ArrayList<>();
+        try {
+            while (rs.next()) {
+                result.add(Map.of("songId", rs.getString(1), "title", rs.getString(2)));
             }
-            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
-    public List<Songs> findSongsByAlbum(Integer albumId)  throws Exception{
-            String query = "SELECT * FROM songs WHERE albumId = " + albumId;
-            ResultSet data = genericDAO.executeQuery(query);
-            List<Songs> result = new ArrayList<>();
-            try {
-                while (data.next()) {
-                    result.add(new Songs(data.getString("songId"), data.getDouble("royaltyRate"), data.getString("title"), data.getString("royaltyStatus"), data.getInt("playCount"), data.getString("country"), data.getString("language"), data.getDouble("duration"), data.getInt("primaryArtist"), data.getInt("albumId")));
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            return result;
-    }
-
-    public  List<Episodes> findPodcastEpisodes(Integer podcastId) throws Exception {
-            String query = "SELECT * FROM episodes WHERE podcastId = "+podcastId;
-            ResultSet data = genericDAO.executeQuery(query);
-            List<Episodes> episodes = new ArrayList<>();
+    public List<Songs> findSongsByAlbum(Integer albumId) throws Exception {
+        String query = "SELECT * FROM songs WHERE albumId = " + albumId;
+        ResultSet data = genericDAO.executeQuery(query);
+        List<Songs> result = new ArrayList<>();
         try {
             while (data.next()) {
-                episodes.add(new Episodes(data.getInt("episodeId"), data.getInt("number"), data.getString("title"), data.getDouble("duration"), data.getString("releaseDate"), data.getInt("ListeningCount"), data.getInt("AdvertisementCount")));
+                result.add(new Songs(data.getString("songId"), data.getDouble("royaltyRate"), data.getString("title"),
+                        data.getString("royaltyStatus"), data.getInt("playCount"), data.getString("country"),
+                        data.getString("language"), data.getDouble("duration"), data.getInt("primaryArtist"),
+                        data.getInt("albumId")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public List<Episodes> findPodcastEpisodes(Integer podcastId) throws Exception {
+        String query = "SELECT * FROM episodes WHERE podcastId = " + podcastId;
+        ResultSet data = genericDAO.executeQuery(query);
+        List<Episodes> episodes = new ArrayList<>();
+        try {
+            while (data.next()) {
+                episodes.add(new Episodes(data.getInt("podcastId"), data.getInt("number"), data.getString("title"),
+                        data.getDouble("duration"), data.getString("releaseDate"), data.getInt("ListeningCount"),
+                        data.getInt("AdvertisementCount")));
             }
         } catch (SQLException e) {
             e.printStackTrace();
